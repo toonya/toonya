c2d22c96f81dfa0c15c73f25a98e9fd2890fd729
toonya <msc5762@me.com>
2014-01-13 16:16:26 +0800

    fix some problem with firefox.
    By setting SVG 100% 100% and calculate the parent’s width and height.
    Get the h. Set html, body width, height 100%.

--- js/fn.js
+++ js/fn.js
@@ -1,7 +1,19 @@
 jQuery(function($) {
+
+	console.log();
+
 	var title = $('#toonya');
+	var svg  = $('svg');
 	var chars = title.children();
 	TweenMax.set(chars, {fill: '#fff'});
+	var width = svg.outerWidth();
+	var height = svg.outerHeight();
+	var h = 735/svg.parent().width()*svg.parent().height();
+	console.log('svg:'+svg.width()+' '+svg.height()+'   h:'+h);
+
+	$('.transform').each(function(i,e) {
+		TweenMax.set($(e), $(e).data());
+	});
 
 	var tl = new TimelineMax(),
 		rough = RoughEase.ease.config({strength:2, clamp:true}),
@@ -12,9 +24,15 @@ jQuery(function($) {
 
 
 	for (i = 0; i < chars.length; i++) {
-		tl.fromTo(chars[i], 2.4, {transformOrigin:"center -160px", z:0.1, rotation:((Math.random() < 0.5) ? 90 : -90)}, {rotation:0, ease:Elastic.easeOut}, 0.3 + i * 0.06);
+		tl.fromTo(chars[i], 3, {transformOrigin:"center -"+h+'px', z:0.1, rotation:((Math.random() < 0.5) ? 90 : -90)}, {rotation:0, ease:Elastic.easeOut}, 0.3 + i * 0.06);
 		tl.to(chars[i], 0.6, {y:96,  ease:Bounce.easeOut}, 3.4 + Math.random() * 0.6);
-		tl.to(chars[i], 0.6, {autoAlpha:0, ease:rough}, 4.5 + Math.random())
-		tl.to(chars[i], 0.6, {autoAlpha:1, ease:rough}, 8 + Math.random());
+		tl.to(chars[i], 0.6, {autoAlpha:0, ease:rough}, 4.5 + Math.random());
+		tl.to(chars[i], 0.6, {autoAlpha:1, ease:rough}, 7 + Math.random());
 	}
+
+	//var tl_title = new TimelineMax();
+	//tl_title.to(title, 1, {x: '+=500', y: '-=500px', rotation: 3600, fillOpacity: 0, scale: 0, ease: },9);
+
+	//TweenMax.to(chars[0], 1, {transformOrigin:'center center', rotation: 360}, 1);
+
 })
