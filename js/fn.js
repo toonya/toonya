70a395ee301df20c67e4c614195fad5118a3a68c
toonya <msc5762@me.com>
2014-01-13 20:03:28 +0800

    Parallax Demo.

--- js/fn.js
+++ js/fn.js
@@ -22,6 +22,9 @@ jQuery(function($) {
 	tl.set(chars, {autoAlpha:1})
 
 
+	console.log(Math.random() * (1.5 - 0.5) + 0.5);
+/*
+
 
 	for (i = 0; i < chars.length; i++) {
 		tl.fromTo(chars[i], 3, {transformOrigin:"center -"+h+'px', z:0.1, rotation:((Math.random() < 0.5) ? 90 : -90)}, {rotation:0, ease:Elastic.easeOut}, 0.3 + i * 0.06);
@@ -29,10 +32,56 @@ jQuery(function($) {
 		tl.to(chars[i], 0.6, {autoAlpha:0, ease:rough}, 4.5 + Math.random());
 		tl.to(chars[i], 0.6, {autoAlpha:1, ease:rough}, 7 + Math.random());
 	}
+*/
+	TweenMax.set(title, {y: '-=200'});
+
+	for (i = 0; i < chars.length; i++) {
+		var z = Math.random() * (1.2 - 0.8) + 0.8;
+		TweenMax.set(chars[i], {transformOrigin:'center center', scale: z*z*z, fill: z * 0xffffff});
+		TweenMax.to(chars[i], 5, {y: '+='+z*400});
+		chars.eq(i).attr('z',z);
+	}
+
+	TweenMax.to($('body'), 5, {backgroundPositionY: '+=5'});
+
+
 
 	//var tl_title = new TimelineMax();
 	//tl_title.to(title, 1, {x: '+=500', y: '-=500px', rotation: 3600, fillOpacity: 0, scale: 0, ease: },9);
 
 	//TweenMax.to(chars[0], 1, {transformOrigin:'center center', rotation: 360}, 1);
+
+	var origin = {
+		x: 0,
+		y: 0
+	}
+	$(window).mousemove(function(e){
+		if(origin.x!=0 && origin.y!=0){
+			var move = {
+				x: (e.pageX - origin.x)*0.1,
+				y: (e.pageY - origin.y)*0.1
+			}
+
+			//console.log(move.x>=0? '+='+move.x:'-='+(0-move.x));
+
+			chars.each(function(i,e){
+				var z = $(e).attr('z');
+				var _move = {
+					x: move.x*z*z*z*z*z,
+					y: move.y*z*z*z*z*z
+				}
+				console.log(_move.x+' '+_move.y);
+
+				TweenMax.set($(e), {x: _move.x>=0? '+='+_move.x:'-='+(0-_move.x), y: _move.y>=0? '+='+_move.y:'-='+(0-_move.y)});
+			})
+
+			TweenMax.set($('body'), {backgroundPositionX: move.x>=0? '+='+move.x*0.05:'-='+(0-move.x*0.05), backgroundPositionY: move.y>=0? '+='+move.y*0.05:'-='+(0-move.y*0.05)});
+
+		}
+
+		origin.x = e.pageX;
+		origin.y = e.pageY;
+
+	})
 
 })
