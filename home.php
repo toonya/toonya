580e02ceb06aa44cc5e50762e29a3e021579921e
toonya <msc5762@me.com>
2014-01-20 19:45:26 +0800

    mobile problem save.

--- home.php
+++ home.php
@@ -12,7 +12,6 @@
         <link href="<?php echo get_template_directory_uri() ?>/Bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="<?php echo get_template_directory_uri(); ?>/css/component.css" rel="stylesheet" media="screen">
         <link href="<?php echo get_stylesheet_directory_uri() ?>/style.css" rel="stylesheet" media="screen">
-        <script src="<?php echo get_template_directory_uri() ?>/js/prefixfree.min.js"></script>
 
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
@@ -21,59 +20,60 @@
 		<![endif]-->
         <?php wp_head(); ?>
     </head>
-    <body class="skrollr-body">
-    	<div class="bg-img" data-start="top:0px; opacity:1" data-100="opacity:0; top:-50px">
-    		<img src="<?php echo get_stylesheet_directory_uri()?>/image/workspace.jpg" width="" height="" alt="" />
+    <body>
+    	<div class="page">
+    		<div class="wrap">
+    			<div class="inner-wrap" id="skrollr-body">
+    				<div class="bg-img" data-start="top:0px; opacity:1" data-100="opacity:0; top:-50px">
+    					<img src="<?php echo get_stylesheet_directory_uri()?>/image/workspace.jpg" width="" height="" alt="" />
+    				</div>
+    				<div data-0="opacity:1" data-100="opacity:0" class="page-aware"><span class="glyphicon glyphicon-chevron-down"></span></div>
+			        <header>
+			        	<div class="wrap">
+			        		<div class="container">
+			        			<div class="service-list">
+			        			        		<h3>我们的服务:</h3>
+			        			        		<div>Logo 标志</div>
+			        			        		<div>Brochure 小册子</div>
+			        			        		<div>Webpage Design 网页设计</div>
+			        			        		<div>Website Develop 网站开发</div>
+			        			</div>
+			        			<h1>TOONYA</h1>
+			        		</div>
+			        	</div>
+			        </header>
+					<h1 data-0="transform:rotateX(90deg)" data-50="transform:rotateX(0deg)" class="text-center section-title">Webpage Design 网页设计</h1>
+					<section>
+						<div class="half item" data-80="opacity:0; transform:translate(-100%)" data-130="opacity:1; transform:translate(0%)">
+							<div class="half-half">
+								<img class="left-top" src="<?php echo get_stylesheet_directory_uri()?>/image/web01.jpg" width="" height="" alt="" />
+							</div>
+							<div class="half-half" data-bottom-top="opacity:0; transform:translate(100%)" data-bottom="opacity:1; transform:translate(0%)">
+								<img class="left-bottom" src="<?php echo get_stylesheet_directory_uri()?>/image/web02.jpg" width="" height="" alt="" />
+							</div>
+						</div>
+						<div class="half item" data-80="opacity:0; transform:translate(100%)" data-130="opacity:1; transform:translate(0%)">
+							<img class="right" src="<?php echo get_stylesheet_directory_uri()?>/image/web03.jpg" width="" height="" alt="" />
+						</div>
+					</section>
+					<h1 data-bottom-top="background:#f15a29;transform:translate(100%)" data-bottom="transform:translate(0)" class="text-center section-title">Logo 标志</h1>
+					<section data-bottom-top="transform:translate(-100%)" data-center-top="transform:translate(0)">
+						<img class="right img-responsive" src="<?php echo get_stylesheet_directory_uri()?>/image/logo01.jpg" width="" height="" alt="" />
+					</section>
+
+
+					<h1 data-bottom-top="background:#fff;transform:translate(-100%)" data-bottom="transform:translate(0)" class="text-center section-title"><a href="mailto:gareth@toonya.me">Email: gareth@toonya.me</a></h1>
+					<footerr>
+						<div class="text-center">
+							<p><?php echo get_option('copyright'); ?></p>
+						</div>
+						<div class="hide">
+							<?php echo get_option('51la'); ?>
+						</div>
+					</footer>
+    			</div>
+    		</div>
     	</div>
-    	<div data-0="opacity:1" data-100="opacity:0" class="page-aware"><span class="glyphicon glyphicon-chevron-down"></span></div>
-
-
-    	<?php if(!is_home()):;?>
-        <div id="startAnimation"></div>
-        <?php endif;?>
-        <header>
-        	<div class="wrap">
-        		<div class="container">
-        			<div class="service-list">
-        			        		<h3>我们的服务:</h3>
-        			        		<div>Logo 标志</div>
-        			        		<div>Brochure 小册子</div>
-        			        		<div>Webpage Design 网页设计</div>
-        			        		<div>Website Develop 网站开发</div>
-        			</div>
-        			<h1>TOONYA</h1>
-        		</div>
-        	</div>
-        </header>
-		<h1  data-0="transform:rotateX(90deg)" data-50="transform:rotateX(0deg)" class="text-center">Webpage Design 网页设计</h1>
-		<section>
-			<div class="half item" data-80="opacity:0; transform:translate(-100%)" data-130="opacity:1; transform:translate(0%)">
-				<div class="half-half">
-					<img class="left-top" src="<?php echo get_stylesheet_directory_uri()?>/image/web01.jpg" width="" height="" alt="" />
-				</div>
-				<div class="half-half" data-bottom-top="opacity:0; transform:translate(100%)" data-bottom="opacity:1; transform:translate(0%)">
-					<img class="left-bottom" src="<?php echo get_stylesheet_directory_uri()?>/image/web02.jpg" width="" height="" alt="" />
-				</div>
-			</div>
-			<div class="half item" data-80="opacity:0; transform:translate(100%)" data-130="opacity:1; transform:translate(0%)">
-				<img class="right" src="<?php echo get_stylesheet_directory_uri()?>/image/web03.jpg" width="" height="" alt="" />
-			</div>
-		</section>
-		<h1 data-bottom-top="background:#f15a29;transform:translate(100%)" data-bottom="transform:translate(0)" class="text-center">Logo 标志</h1>
-		<section data-bottom-top="transform:translate(-100%)" data-center-top="transform:translate(0)">
-			<img class="right img-responsive" src="<?php echo get_stylesheet_directory_uri()?>/image/logo01.jpg" width="" height="" alt="" />
-		</section>
-
-
-		<h1 data-bottom-top="background:#fff;transform:translate(-100%)" data-bottom="transform:translate(0)" class="text-center"><a href="mailto:gareth@toonya.me">Email: gareth@toonya.me</a></h1>
-		<footerr>
-			<div class="text-center">
-				<p><?php echo get_option('copyright'); ?></p>
-			</div>
-			<div class="hide">
-				<?php echo get_option('51la'); ?>
-			</div>
-		</footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <!-- //code.jquery.com/jquery.js -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
@@ -87,7 +87,7 @@
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/skrollr/dist/skrollr.min.js"></script>
 
 	<script type="text/javascript">
-	skrollr.init({forceHeight: false});
+	skrollr.init({forceHeight: true});
 	</script>
     <?php wp_footer();?>
   </body>
