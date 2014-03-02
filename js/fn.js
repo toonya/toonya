24674bae45f671e0320822dbc56e590d3ba3ce86
toonya <msc5762@me.com>
2014-03-02 19:50:36 +0800

    1

--- js/fn.js
+++ js/fn.js
@@ -1,6 +1,8 @@
 jQuery(function($) {
 
 	skrollr.init({forceHeight: true,smoothScrolling: true});
+
+	$('.scrollr-over-hidden').css('overflowX','hidden');
 /*
 	function vertical_check() {
 		if($(window).width()<$(window).height())
